ba884771aab32d2a3a9a7dc5f27fa568f782b38b
dishantbhardwaj320 <63861681+dishantbhardwaj320@users.noreply.github.com>
2020-08-15 01:29:48 +0530

    Add files via upload
    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("connected");
+
+
+
+
+var image = document.getElementById("myImage");
+image.addEventListener("click", function (){
+	var a = document.getElementById("J");
+	a.play();
+	image.src = "hosting1.gif"
+	console.log("Music");
+});
